b5c69c7cb01e8190bb7affe2f652110cc3095d43
Keegan De souza <124557397+KeeganDesouza@users.noreply.github.com>
2024-10-21 17:03:47 +0100

    Gui
    Modified the code to Start on the GUI

--- system.py
+++ system.py
@@ -1,6 +1,16 @@
+import tkinter as tk
+
 symptom1 = input("Symptom1 > ")
 symptom2 = input("Symptom2 > ")
 symptom3 = input("Symptom3 > ")
 
 
 print(f"{symptom1}, {symptom2}, {symptom3}")
+
+
+
+root = tk.Tk()
+root.title("Symptom Checker")
+root.geometry("400x300") 
+
+root.mainloop()
